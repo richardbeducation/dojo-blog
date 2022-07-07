ad028e9eea03f5be9c28ea921d1d4789986f1fc7
richardbeducation <richardbeducation@gmail.com>
2022-07-07 15:29:05 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Navbar from './Navbar'
 import Home from './Home'
 
-
 function App() {
   return (
     <div className="App">
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+  const handleClick = () => {
+    console.log('hello ninjas')
+  }
+
+  const handleClickAgain = (name) => {
+    console.log('hello ' + name);
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={handleClickAgain}>Click me</button>
     </div>   
   );
 }
