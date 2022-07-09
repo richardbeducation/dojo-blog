45e9a1c333a187c547e163e6737b52c4ba633c9b
richardbeducation <richardbeducation@gmail.com>
2022-07-09 16:11:07 +0700

    update

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,17 @@
+import { useState } from "react"
+import BlogList from "./BlogList"
+
 const Home = () => {
-
-  const handleClick = () => {
-    console.log('hello ninjas')
-  }
-
-  const handleClickAgain = (name) => {
-    console.log('hello ' + name);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]) 
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={handleClickAgain}>Click me</button>
-    </div>   
+      <BlogList blogs={ blogs }/>
+    </div>     
   );
 }
 
